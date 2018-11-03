a2243d5c9cdf4df37fae795cd32d197831d55c65
Alain Mosnier <alain@wanamoon.net>
2018-11-03 18:49:09 +0100

    Added more Ivy settings, and keyboard shortcuts.

--- init.el
+++ init.el
@@ -78,11 +78,30 @@
 ;; Activate Ivy mode
 (require 'ivy)
 (ivy-mode 1)
+(counsel-mode 1)
 (setq ivy-count-format "(%d/%d) ")
+(setq ivy-use-virtual-buffers t)
+(setq enable-recursive-minibuffers t)
 
-;; Keyboard shortcuts
+;; Ivy keyboard shortcuts
 (global-set-key "\C-s" 'swiper)
+(global-set-key (kbd "C-c C-r") 'ivy-resume)
+(global-set-key (kbd "<f6>") 'ivy-resume)
 (global-set-key (kbd "M-x") 'counsel-M-x)
+(global-set-key (kbd "C-x C-f") 'counsel-find-file)
+(global-set-key (kbd "<f1> f") 'counsel-describe-function)
+(global-set-key (kbd "<f1> v") 'counsel-describe-variable)
+(global-set-key (kbd "<f1> l") 'counsel-find-library)
+(global-set-key (kbd "<f2> i") 'counsel-info-lookup-symbol)
+(global-set-key (kbd "<f2> u") 'counsel-unicode-char)
+(global-set-key (kbd "<f11>") 'counsel-imenu)
+(global-set-key (kbd "C-c g") 'counsel-git)
+(global-set-key (kbd "C-c j") 'counsel-git-grep)
+(global-set-key (kbd "C-c k") 'counsel-ag)
+(global-set-key (kbd "C-x l") 'counsel-locate)
+(define-key minibuffer-local-map (kbd "C-r") 'counsel-minibuffer-history)
+
+;; General keyboard shortcuts
 (global-set-key [f7] 'compile)
 (global-set-key [f8] 'recompile)
 (global-set-key [f9] 'previous-error)
