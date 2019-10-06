f4e3dfb3d6c43d572ba3040d32b3fc7f14d23c92
Alain Mosnier <alain@wanamoon.net>
2019-10-06 15:40:36 +0200

    Updated packages and added haskell-mode.

---
+++ elpa/counsel-20191005.1707/counsel-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "counsel" "20191005.1707" "Various completion functions using Ivy" '((emacs "24.3") (swiper "0.12.0")) :commit "30adc93d5a7745463bb1f3c6905aead59e5f6be9" :keywords '("convenience" "matching" "tools") :authors '(("Oleh Krehel" . "ohwoeowho@gmail.com")) :maintainer '("Oleh Krehel" . "ohwoeowho@gmail.com") :url "https://github.com/abo-abo/swiper")
