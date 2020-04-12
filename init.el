fb127371f48d0a69c24bd0f09849e588a9766074
Alain Mosnier <alain@wanamoon.net>
2020-04-12 11:50:22 +0200

    Added htmlize. Good for org-mode.

--- init.el
+++ init.el
@@ -149,17 +149,3 @@ There are two things you can do about this warning:
 (provide 'init)
 
 ;;; init.el ends here
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (zenburn-theme smart-tabs-mode powerline markdown-mode haskell-mode gnu-elpa-keyring-update glsl-mode git-commit counsel company cmake-mode))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
